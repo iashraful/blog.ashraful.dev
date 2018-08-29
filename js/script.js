de34d81cfce162f9ccc37cf8301bf9158f813d84
Ashraful Islam <ashraful@field.buzz>
2018-08-29 09:04:19 +0600

    * Removed unnecessary files * Removed medium blog posts embading

--- js/script.js
+++ js/script.js
@@ -34,34 +34,4 @@
 
     $('.details-wrapper .tab-pane').mCustomScrollbar();
 
-    $(document).ready(function() {
-        // Get latest post from medium
-        // Using... https://rss2json.com
-        // Random Number is just not for caching the api on that url. I want to fetch every time
-        var randomNumber = parseInt(Math.random() * 1000)
-        var username = '_ashraful'
-        var url = "https://medium.com/feed/@" + username + '?v=' + randomNumber;
-        var converterUrl = "https://api.rss2json.com/v1/api.json?rss_url=" + url;
-
-        $.get(converterUrl, function(data) {
-            console.log(data)
-            produceBlogpost(data.items);
-        });
-
-
-        // Produce blog post
-        var produceBlogpost = function(posts) {
-            var blogPostsUL = $('#blog-posts-ul');
-            var postBody = '';
-            for (var i=0; i<posts.length; i++) {
-                var postDesc = posts[i].description;
-                postBody += '<li class="blog-post">' + postDesc.slice(0, 500) + '...';
-                postBody += '<a href="'+ posts[i].link +'" target="_blank">Read More</a></li>'
-                // postBody += '<h4>' + posts[i].title +'</h4>'
-            }
-            blogPostsUL.fadeIn().html(postBody);
-        };
-    })
-
-
 })(jQuery); 
