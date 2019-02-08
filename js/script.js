89a18f2640871b40cee6ef0165fb9cdc8799dc6e
Ashraful Islam <ashrafulrobin3@gmail.com>
2019-02-08 11:02:04 +0600

    fixed typo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 (function($) {
     'use strict';
 
-
     // mixItUp
     $( ".filters-menu li a" ).on('click' , function( event ) {
         event.preventDefault();
@@ -16,7 +15,7 @@
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
         var target = $(this).attr('href');
         var container = $('#container')
-        if (target === "#portfo") {
+        if (target === "#portfolio") {
           if (!container.mixItUp('isLoaded')) {
             container.mixItUp();
           }
