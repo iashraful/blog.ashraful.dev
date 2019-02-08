5b10f59e638dc98e8e47369712d30050f403a594
Ashraful Islam <ashrafulrobin3@gmail.com>
2019-02-08 10:51:24 +0600

    * revert back to rss2json from Medium blog * fixed multilple feed in a day with rss key

--- js/script.js
+++ js/script.js
@@ -34,4 +34,34 @@
 
     $('.details-wrapper .tab-pane').mCustomScrollbar();
 
+    $(document).ready(function() {
+        // Get latest post from medium
+        // Using... https://rss2json.com
+        // Random Number is just not for caching the api on that url. I want to fetch every time
+        var randomNumber = parseInt(Math.random() * 1000)
+        var username = '_ashraful'
+        var url = "https://medium.com/feed/@" + username + '?v=' + randomNumber;
+        var converterUrl = "https://api.rss2json.com/v1/api.json?rss_url=" + url + '&api_key=mtl7nzy5pjnyztt6rjgmzkalcjzumf7cqpxjkmg7';
+
+        $.get(converterUrl, function(data) {
+            console.log(data)
+            produceBlogpost(data.items);
+        });
+
+
+        // Produce blog post
+        var produceBlogpost = function(posts) {
+            var blogPostsUL = $('#blog-posts-ul');
+            var postBody = '<div class="medium-posts">';
+            for (var i=0; i<posts.length; i++) {
+                var postDesc = posts[i].description;
+                postBody += '<h2>' + posts[i].title +'</h2><br/>'
+                postBody += '<li class="blog-post">' + postDesc.slice(0, 500) + '...</br>';
+                postBody += '<a class="read-more-btn" href="'+ posts[i].link +'" target="_blank">Read More</a></li>'
+            }
+            postBody += '</div>'
+            blogPostsUL.fadeIn().html(postBody);
+        };
+    })
+
 })(jQuery); 
