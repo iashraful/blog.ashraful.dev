9d031159bdf577bf73893e8614b580691cb7532f
Ashraful Islam <ashrafulrobin3@gmail.com>
2019-10-26 13:47:51 +0600

    Added special height for social button to stop blink

--- src/.vuepress/theme/components/SocialButton.vue
+++ src/.vuepress/theme/components/SocialButton.vue
@@ -1,56 +1,57 @@
 <template>
-    <div>
+    <div style="height: 50px">
         <a class="social-link"
-            target="_blank"
-            :href="link.link" 
-            v-for="link in socialLinks" 
-            :key="link.link">
-            <font-awesome-icon :icon="link.icon" size="3x"/>
+           target="_blank"
+           :href="link.link"
+           v-for="link in socialLinks"
+           :key="link.link">
+            <font-awesome-icon style="height: 50px" :icon="link.icon" size="3x"/>
         </a>
     </div>
 </template>
 
 
 <script>
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { 
-    faFacebookSquare, faTwitterSquare, faLinkedin, faStackOverflow, faGithubSquare
-} from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(faFacebookSquare, faTwitterSquare, faLinkedin, faStackOverflow, faGithubSquare)
+    import {library} from '@fortawesome/fontawesome-svg-core'
+    import {
+        faFacebookSquare, faTwitterSquare, faLinkedin, faStackOverflow, faGithubSquare
+    } from '@fortawesome/free-brands-svg-icons'
+    import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
-export default {
-    name: 'SocialButton',
-    components: {
-        FontAwesomeIcon
-    },
-    data() {
-        return {
-            socialLinks: {
-                'github': {
-                    'icon': ['fab', 'github-square'],
-                    'link': 'https://github.com/iashraful'
-                },
-                'linkedin': {
-                    'icon': ['fab', 'linkedin'],
-                    'link': 'https://www.linkedin.com/in/~ashraful/'
-                },
-                'twitter': {
-                    'icon': ['fab', 'twitter-square'],
-                    'link': 'https://twitter.com/__ashraful'
-                },
-                'stackoverflow': {
-                    'icon': ['fab', 'stack-overflow'],
-                    'link': 'https://stackoverflow.com/users/5527679/ashraful-islam'
-                },
-                'facebook': {
-                    'icon': ['fab', 'facebook-square'],
-                    'link': 'https://facebook.com/ashraful.py'
-                },
+    library.add(faFacebookSquare, faTwitterSquare, faLinkedin, faStackOverflow, faGithubSquare)
+
+    export default {
+        name: 'SocialButton',
+        components: {
+            FontAwesomeIcon
+        },
+        data() {
+            return {
+                socialLinks: {
+                    'github': {
+                        'icon': ['fab', 'github-square'],
+                        'link': 'https://github.com/iashraful'
+                    },
+                    'linkedin': {
+                        'icon': ['fab', 'linkedin'],
+                        'link': 'https://www.linkedin.com/in/~ashraful/'
+                    },
+                    'twitter': {
+                        'icon': ['fab', 'twitter-square'],
+                        'link': 'https://twitter.com/__ashraful'
+                    },
+                    'stackoverflow': {
+                        'icon': ['fab', 'stack-overflow'],
+                        'link': 'https://stackoverflow.com/users/5527679/ashraful-islam'
+                    },
+                    'facebook': {
+                        'icon': ['fab', 'facebook-square'],
+                        'link': 'https://facebook.com/ashraful.py'
+                    },
+                }
             }
         }
     }
-}
 </script>
 
 
